251c9ec994cdf3bb4d15bb70511e7acbed9abc10
Celine Te <celine.te.mcgill@gmail.com>
2018-08-08 16:16:11 -0400

    Review and Restaurant all green

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,8 @@
+class RestaurantsController < ApplicationController
+#   @restaurants = Restaurant.all
+#     validates :name, presence: true
+# end
+
+# Person.create(name: "John Doe").valid? # => true
+# Person.create(name: nil).valid? # => false
+end
