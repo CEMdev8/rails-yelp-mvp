a5ffe5ec1ce65271063cc90112b6e8e71e644148
Celine Te <celine.te.mcgill@gmail.com>
2018-08-09 13:23:57 -0400

    rails new

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,4 +5,25 @@ class RestaurantsController < ApplicationController
 
 # Person.create(name: "John Doe").valid? # => true
 # Person.create(name: nil).valid? # => false
+
+  def index
+      @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurants =
+  end
+
+  def new
+    @restaurants = Restaurant.new
+  end
+
+  def create
+    @restaurants = Restaurant.create
+  end
+
+  def edit
+  end
+
+  def restaurant_params
 end
